29d96b5df0689dce60a92ff138738badec8adfee
jonmcfee03 <jmcfee@umich.edu>
2023-09-28 17:02:40 -0400

    bruh

--- src/App.js
+++ src/App.js
@@ -5,33 +5,51 @@ import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className="page">
+    <div className="App">
       <div className="navbar">
-        <Navbar />
+          <Navbar />
       </div>
-      <div className="title-page">
-        <div id="home" className="about">
-          <div className="div-name">
-            <span className="name">
-              JONATHAN<br/>MCFEE
-            </span>
+      <div className="pages">
+        <div id="home" className="page">
+          <div className="home-about">
+            <div className="div-name">
+              <span className="name">
+                JONATHAN<br/>MCFEE
+              </span>
+            </div>
+            <div className="bio">
+            There's the quietus makes, puzzlesh is heart-ache pative hue of the 
+            undispriz'd long end swear that merit 
+            office, to say weat dreams and to, 'tis
+            he oppresolution. Thus count with the
+            sling afterpriz'd coward to sleep:
+            perchance of deat under dels bear thing
+            </div>
           </div>
-          <div className="bio">
-          There's the quietus makes, puzzlesh is heart-ache pative hue of the 
-          undispriz'd long end swear that merit 
-          office, to say weat dreams and to, 'tis
-          he oppresolution. Thus count with the
-          sling afterpriz'd coward to sleep:
-          perchance of deat under dels bear thing
+          <div className="home-links">
+            <ul className="links-list">
+              <li className="home-link">ABOUT</li>
+              <li className="home-link">SKILLS</li>
+              <li className="home-link">PROJECTS</li>
+              <li className="home-link">CONTACT</li>
+            </ul>
           </div>
         </div>
-        <div className="home-links">
-          <ul className="links-list">
-            <li className="home-link">ABOUT</li>
-            <li className="home-link">SKILLS</li>
-            <li className="home-link">PROJECTS</li>
-            <li className="home-link">CONTACT</li>
-          </ul>
+        <div id="about" className="page">
+          <div className="headshot">
+            <img src={require("./images/headshot.jpg")} alt="My Headshot" 
+            className="headshot" />
+          </div>
+          <div className='about-me'>
+            <div className="about-me-title">About Me</div>
+            <div className="about-me-info">
+            There's the quietus makes, puzzlesh is heart-ache pative 
+            hue of the undispriz'd long end swear that merit office, 
+            to say weat dreams and to, 'tis he oppresolution. Thus count 
+            with the sling afterpriz'd coward to sleep: perchance of 
+            deat under dels bear thing
+            </div>
+          </div>
         </div>
       </div>
     </div>
